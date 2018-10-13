9dc33de6234fd5bbbe2b5a4010fee8859d725a93
kying832 <kying832@gmail.com>
2018-10-13 15:08:48 -0700

    Added bugs into the 5 functions
    Added bugs into the 5 functions

--- projects/yingk/dominion/card_functions.c
+++ projects/yingk/dominion/card_functions.c
@@ -6,28 +6,8 @@
 #include <stdlib.h>
 #include "card_functions.h"
 /*
-while (drawntreasure < 2)
-{
-    if (state->deckCount[currentPlayer] < 1)
-    { //if the deck is empty we need to shuffle discard and add to deck
-        shuffle(currentPlayer, state);
-    }
-    drawCard(currentPlayer, state);
-    cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] - 1]; //top card of hand is most recently drawn card.
-    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-        drawntreasure++;
-    else
-    {
-        temphand[z] = cardDrawn;
-        state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-        z++;
-    }
-}
-while (z - 1 >= 0)
-{
-    state->discard[currentPlayer][state->discardCount[currentPlayer]++] = temphand[z - 1]; // discard all cards in play that have been drawn
-    z = z - 1;
-}
+THESE FUNCTIONS ARE THE VERSION WITHOUT BUGS.  THOSE PLACED IN DOMINION.C ARE THE BUGGED ONES.
+
 */
 //parameters are gamestate state struct, int currentPlayer, int drawnTreasure, int [] tempHand, 
 void adventurer_card(struct gameState * state, int currentPlayer, int drawnTreasure, int temp_counter,  int temp_hand[])
--- projects/yingk/dominion/card_functions.h
+++ projects/yingk/dominion/card_functions.h
@@ -6,4 +6,8 @@ void adventurer_card(struct gameState *, int, int, int,  int[] );
 //parameters are state struct, int currentPlayer, int handPos
 void smithy_card(struct gameState *, int, int);
 void village_card(struct gameState *, int, int);
+//parameters are state struct, int currentPlayer, int card choice, int handPos
+void salvager_card(struct gameState*, int, int, int);
+//parameters are state struct, int currentPlayer, int handPos
+int treasure_map_card(struct gameState*, int, int);
 #endif
